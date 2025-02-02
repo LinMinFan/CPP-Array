51f575708bbfb80831e0092277afb2119f598480
JACK LIN <cvse00566@gmail.com>
2025-02-02 16:32:40 +0800

    動態陣列

--- main.cpp
+++ main.cpp
@@ -2,35 +2,29 @@
 using namespace std;
 
 int main() {
-    // Fibonacci Sequence 小於等於 輸入值且不超過 100 個的費氏數列
-    int fs[40] = {1, 1};
-    int max;
+    int **ptr, row, column;
 
-    cin >> max;
-    cout << "1: 1" << endl;
-    cout << "2: 1" << endl;
+    cout << "enter row limit" << endl;
+    cin >> row;
 
-    for (int i = 2; i < 40; i++)
+    cout << "enter column limit" << endl;
+    cin >> column;
+
+    ptr = new int *[row];
+    for (int i = 0; i < row; i++)
     {
-        if (i >= max)
+        ptr[i] = new int [column];
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        cout << i << " => " << endl;
+        for (int j = 0; j < column; j++)
         {
-            break;
+            ptr[i][j] = (i * 10) + j;
+            cout << j << " => " << ptr[i][j] << "," << endl;
         }
-        fs[i] = fs[i - 1] + fs[i - 2];
-        cout << (i + 1) << ": " << fs[i] << endl;
+        
     }
-    
-    // 印出範圍內的費氏數列值
-    int start, end;
-    cin >> start >> end;
-    cout << "start: ";
-
-    for (int i = (start - 1); i < end; i++)
-    {
-        cout << fs[i] << " ";
-    }
-
-    cout << "end";
-    
     return 0;
 }
